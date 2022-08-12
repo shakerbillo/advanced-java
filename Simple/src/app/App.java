e2edd2ca60dba7c41946d7354f79b769a20faf6a
Emmanuel Gyan-Amoah <shakerbillo@yahoo.com>
2022-08-12 17:15:19 +0200

    changed nessage

--- Simple/src/app/App.java
+++ Simple/src/app/App.java
@@ -1,7 +1,7 @@
 package app;
 
-public class App{
-    public static void main(String [] args){
-        System.out.println("It's Shaker. Let's get it!!!!!");
+public class App {
+    public static void main(String[] args) {
+        System.out.println("It's Shaker. Let's get it!!!!! Unconditional Love");
     }
 }
